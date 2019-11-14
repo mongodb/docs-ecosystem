c9352d344db88225db9d88c8c8455f49ca3fe68f
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2019-11-14 16:35:30 -0500

    cleanup java code

--- source/includes/connection-snippets/x509-snippets/java-connection.java
+++ source/includes/connection-snippets/x509-snippets/java-connection.java
@@ -1,10 +1,7 @@
-// begin  x509 connection
-package com.java.mongodb;
 
+// begin x509 connection
 import java.util.Arrays;
-
 import org.bson.Document;
-
 import com.mongodb.MongoClientSettings;
 import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
@@ -12,30 +9,20 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoIterable;
 
-public class MyDemo {
+System.setProperty("javax.net.ssl.trustStore","etc/certs/mongodb/client.truststore");
+System.setProperty("javax.net.ssl.trustStorePassword","<pass>");
 
-    public static void main(String args[]) {
+System.setProperty("javax.net.ssl.keyStore","etc/certs/mongodb/client.keystore");
+System.setProperty("javax.net.ssl.keyStorePassword","<pass>");
 
-        System.setProperty("javax.net.ssl.trustStore", "/Users/<user>/dev/drivers/certs/v3/client.truststore");
-        System.setProperty("javax.net.ssl.trustStorePassword", "<pass>");
-
-        System.setProperty("javax.net.ssl.keyStore", "/Users/<user>/dev/drivers/certs/v3/client.keystore");
-        System.setProperty("javax.net.ssl.keyStorePassword", "<pass>");
-
-        try {
-            String subject = "<your_subject>"; // CN=ChrisChoClient,OU=TestClientCertificateOrgUnit,O=TestClientCertificateOrg,L=TestClientCertificateLocality,ST=TestClientCertificateState,C=US
-            MongoCredential credential = MongoCredential.createMongoX509Credential(subject);
-
-            MongoClientSettings settings = MongoClientSettings.builder().credential(credential)
-                    .applyToClusterSettings(builder -> builder.hosts(Arrays.asList(new ServerAddress("localmongo1"))))
-                    .applyToSslSettings(builder -> builder.enabled(true)).build();
-
-            MongoClient client = MongoClients.create(settings);
-            client.close();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-
-    }
+try{
+    String subject="<your_subject>"; // CN=ChrisChoClient,OU=TestClientCertificateOrgUnit,O=TestClientCertificateOrg,L=TestClientCertificateLocality,ST=TestClientCertificateState,C=US
+    MongoCredential credential= MongoCredential.createMongoX509Credential(subject);
+    MongoClientSettings settings=MongoClientSettings.builder().credential(credential).applyToClusterSettings(builder->builder.hosts(Arrays.asList(new ServerAddress("localhost")))).applyToSslSettings(builder->builder.enabled(true)).build();
+    MongoClient client=MongoClients.create(settings);
+    client.close();
+}catch(Exception e){
+    System.out.println(e);
 }
+
 // end x509 connection
