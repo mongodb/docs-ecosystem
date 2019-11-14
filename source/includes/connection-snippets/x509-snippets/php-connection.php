f9a9c17ef4dd9422bfada3b75d22793fce2963fc
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2019-11-14 16:46:45 -0500

    fixed php example

--- source/includes/connection-snippets/x509-snippets/php-connection.php
+++ source/includes/connection-snippets/x509-snippets/php-connection.php
@@ -4,11 +4,11 @@
 $database = 'test';
 $subject = "mongodb://<your_subject>"; // mongodb://CN=<host>,OU=TestClientCertificateOrgUnit,O=TestClientCertificateOrg,L=TestClientCertificateLocality,ST=TestClientCertificateState,C=US
 $manager = new MongoDB\Driver\Manager(
-  $subject + "@localmongo1/?ssl=true&authMechanism=MONGODB-X509",
+  $subject + "@localhost/?ssl=true&authMechanism=MONGODB-X509",
   [],
   [
-    "pem_file" => "/Users/<user>/dev/drivers/certs/v3/test-client.pem",
-    "ca_file" => "/Users/<user>/dev/drivers/certs/v3/test-ca.pem",
+    "pem_file" => "/etc/certs/client.pem",
+    "ca_file" => "/etc/certs/ca.pem",
   ]
 );
 ?>
