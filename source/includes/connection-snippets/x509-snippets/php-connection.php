805d43f8df11304d07605a6bf5de8ddd2b5bddeb
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2019-11-14 16:47:47 -0500

    fixed py example

--- source/includes/connection-snippets/x509-snippets/php-connection.php
+++ source/includes/connection-snippets/x509-snippets/php-connection.php
@@ -1,14 +1,13 @@
 <!-- Begin x509 connection -->
 <?php
 
-$database = 'test';
 $subject = "mongodb://<your_subject>"; // mongodb://CN=<host>,OU=TestClientCertificateOrgUnit,O=TestClientCertificateOrg,L=TestClientCertificateLocality,ST=TestClientCertificateState,C=US
 $manager = new MongoDB\Driver\Manager(
   $subject + "@localhost/?ssl=true&authMechanism=MONGODB-X509",
   [],
   [
-    "pem_file" => "/etc/certs/client.pem",
-    "ca_file" => "/etc/certs/ca.pem",
+    "pem_file" => "/etc/certs/mongodb/client.pem",
+    "ca_file" => "/etc/certs/mongodb/ca.pem",
   ]
 );
 ?>
--- source/includes/connection-snippets/x509-snippets/python-connection.py
+++ source/includes/connection-snippets/x509-snippets/python-connection.py
@@ -4,9 +4,8 @@ from pymongo import MongoClient
 
 uri = "mongodb://localmongo1/?authMechanism=MONGODB-X509"
 client = MongoClient(uri,
-                        ssl=True,
-                        ssl_certfile='/test-client.pem',
-                        ssl_cert_reqs=ssl.CERT_REQUIRED,
-                        ssl_ca_certs='/test-ca.pem')
-
+ssl=True,
+ssl_certfile='/etc/certs/mongodb/client.pem',
+ssl_cert_reqs=ssl.CERT_REQUIRED,
+ssl_ca_certs='/etc/certs/mongodb/ca.pem')
 # end x509 connection
