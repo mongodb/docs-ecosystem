08c8a78b033e3d2dac40d091a3d6027fa24a375a
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2019-11-14 16:49:52 -0500

    fixed scala example

--- source/includes/connection-snippets/x509-snippets/scala-connection.scala
+++ source/includes/connection-snippets/x509-snippets/scala-connection.scala
@@ -7,26 +7,20 @@ import org.mongodb.scala.bson.collection.mutable.Document
 import org.mongodb.scala.bson.BsonString
 import com.mongodb.client.model.FindOneAndUpdateOptions
 
-object MongoDBx509 extends App {
-   System.setProperty("javax.net.ssl.trustStore","client.truststore");
-   System.setProperty("javax.net.ssl.trustStorePassword","password");
+System.setProperty("javax.net.ssl.trustStore","client.truststore");
+System.setProperty("javax.net.ssl.trustStorePassword","<your_password>");
+System.setProperty("javax.net.ssl.keyStore", "client.keystore");
+System.setProperty("javax.net.ssl.keyStorePassword", "<your_password>");
 
-   System.setProperty("javax.net.ssl.keyStore", "client.keystore");
-   System.setProperty("javax.net.ssl.keyStorePassword", "password");
-
-   // null value will force mongoDB server to use DN from cert
-   val credential = MongoCredential.createMongoX509Credential(null);
-
-   val settings = MongoClientSettings.builder()
-      .credential(credential)
-      .applyToClusterSettings(
-         (builder: ClusterSettings.Builder)
-         => builder.hosts(List(new ServerAddress("localmongo1", 27017)).asJava))
-      .applyToSslSettings(
-         (builder : SslSettings.Builder)
-         => builder.enabled(true))
-      .build();
-
-   val client : MongoClient = MongoClient(settings);
-}
+val credential = MongoCredential.createMongoX509Credential(null);
+val settings = MongoClientSettings.builder()
+   .credential(credential)
+   .applyToClusterSettings(
+      (builder: ClusterSettings.Builder)
+      => builder.hosts(List(new ServerAddress("localhost", 27017)).asJava))
+   .applyToSslSettings(
+      (builder : SslSettings.Builder)
+      => builder.enabled(true))
+   .build();
+val client : MongoClient = MongoClient(settings);
 // end x509 connection
