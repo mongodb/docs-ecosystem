1c51bc482692c77615766e71b85f3bb6a371c3a6
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2019-11-14 16:30:01 -0500

    cleaned up go exmple

--- source/includes/connection-snippets/x509-snippets/go-connection.go
+++ source/includes/connection-snippets/x509-snippets/go-connection.go
@@ -9,25 +9,19 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
+ctx := context.TODO()
+caFilePath := "ca.pem"
+certificateKeyFilePath := "client.pem"
+uri := "mongodb://localhost:27017/?tlsCAFile=%s&tlsCertificateKeyFile=%s&authMechanism=MONGODB-X509"
+uri = fmt.Sprintf(uri, caFilePath, certificateKeyFilePath)
+clientOpts := options.Client().ApplyURI(uri).SetServerSelectionTimeout(2 * time.Second)
+client, err := mongo.Connect(ctx, clientOpts)
+if err != nil {
+	log.Fatal(err)
+}
 
-func main() {
-	ctx := context.TODO()
-	caFilePath := "ca.pem"
-	certificateKeyFilePath := "client.pem"
-
-	uri := "mongodb://localhost:27017/?tlsCAFile=%s&tlsCertificateKeyFile=%s&authMechanism=MONGODB-X509"
-	uri = fmt.Sprintf(uri, caFilePath, certificateKeyFilePath)
-	clientOpts := options.Client().ApplyURI(uri).SetServerSelectionTimeout(2 * time.Second)
-
-	client, err := mongo.Connect(ctx, clientOpts)
-	if err != nil {
-		log.Fatal(err)
-	}
-	
-	defer client.Disconnect(ctx)
-
-	if err = client.Ping(ctx, nil); err != nil {
-		log.Fatal(err)
-	}
+defer client.Disconnect(ctx)
+if err = client.Ping(ctx, nil); err != nil {
+	log.Fatal(err)
 }
 // end x509 connection
