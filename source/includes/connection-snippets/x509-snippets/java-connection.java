a1b90d68f54acc3f94668df61bcf04ad58f69fe7
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2019-11-14 16:41:28 -0500

    fixed java code x509

--- source/includes/connection-snippets/x509-snippets/java-connection.java
+++ source/includes/connection-snippets/x509-snippets/java-connection.java
@@ -9,20 +9,17 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoIterable;
 
-System.setProperty("javax.net.ssl.trustStore","etc/certs/mongodb/client.truststore");
-System.setProperty("javax.net.ssl.trustStorePassword","<pass>");
 
-System.setProperty("javax.net.ssl.keyStore","etc/certs/mongodb/client.keystore");
-System.setProperty("javax.net.ssl.keyStorePassword","<pass>");
+System.setProperty("javax.net.ssl.trustStore","/etc/certs/mongodb/client.truststore");
+System.setProperty("javax.net.ssl.trustStorePassword","mypass");
 
-try{
-    String subject="<your_subject>"; // CN=ChrisChoClient,OU=TestClientCertificateOrgUnit,O=TestClientCertificateOrg,L=TestClientCertificateLocality,ST=TestClientCertificateState,C=US
-    MongoCredential credential= MongoCredential.createMongoX509Credential(subject);
-    MongoClientSettings settings=MongoClientSettings.builder().credential(credential).applyToClusterSettings(builder->builder.hosts(Arrays.asList(new ServerAddress("localhost")))).applyToSslSettings(builder->builder.enabled(true)).build();
-    MongoClient client=MongoClients.create(settings);
-    client.close();
-}catch(Exception e){
-    System.out.println(e);
-}
-
+System.setProperty("javax.net.ssl.keyStore", "/etc/certs/mongodb/client.keystore");
+System.setProperty("javax.net.ssl.keyStorePassword", "mypass");
+String subject = "<your_subject>"; // CN=ChrisChoClient,OU=TestClientCertificateOrgUnit,O=TestClientCertificateOrg,L=TestClientCertificateLocality,ST=TestClientCertificateStateC=US
+MongoCredential credential = MongoCredential.createMongoX509Credential(subject); 
+MongoClientSettings settings = MongoClientSettings.builder()
+        .credential(credential)
+        .applyToClusterSettings(builder -> builder.hosts(Arrays.asList(new ServerAddress("localhost"))))
+        .applyToSslSettings(builder -> builder.enabled(true)).build();
+MongoClient client = MongoClients.create(settings);
 // end x509 connection
