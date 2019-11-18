eb9008feb6b1356edd0a7f3bd8b1aebd9128befe
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2019-11-18 12:35:30 -0500

    fixed spacing

--- source/includes/connection-snippets/x509-snippets/php-connection.php
+++ source/includes/connection-snippets/x509-snippets/php-connection.php
@@ -1,6 +1,13 @@
 <!-- Begin x509 connection -->
 <?php
-$subject = "mongodb+srv://<your_subject>"; // mongodb+srv://CN=<host>,OU=TestClientCertificateOrgUnit,O=TestClientCertificateOrg,L=TestClientCertificateLocality,ST=TestClientCertificateState,C=US
-$manager = new MongoDB\Driver\Manager($subject + "@<cluster-url>/?ssl=true&authMechanism=MONGODB-X509", [], ["pem_file" => "/etc/certs/mongodb/client.pem", "ca_file" => "/etc/certs/mongodb/ca.pem", ]);
+$subject = "mongodb+srv://<your_subject>"; 
+$manager = new MongoDB\Driver\Manager(
+    $subject + "@<cluster-url>/?ssl=true&authMechanism=MONGODB-X509",
+  [],
+  [
+    "pem_file" => "/etc/certs/mongodb/v3/client.pem",
+    "ca_file" => "/etc/certs/mongodb/v3/ca.pem",
+  ]
+);
 ?>
 <!-- End x509 connection -->
