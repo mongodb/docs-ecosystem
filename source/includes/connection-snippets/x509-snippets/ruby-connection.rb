963017be48cf91c2fdb026d49126bbeae3df9772
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2019-11-18 12:30:30 -0500

    ran ruby code through rubocop

--- source/includes/connection-snippets/x509-snippets/ruby-connection.rb
+++ source/includes/connection-snippets/x509-snippets/ruby-connection.rb
@@ -1,15 +1,14 @@
-#begin x509 connection
+# begin x509 connection
 require 'mongo'
 
 begin
-client = Mongo::Client.new(
-  ['localmongo1:27017'],
-  auth_mech:: mongodb_x509,
-  ssl: true,
-  ssl_cert: '/etc/certs/mongodb/client.pem',
-  ssl_key: '/etc/certs/mongodb/client.pem',
-  ssl_ca_cert: '/etc/certs/mongodb/ca.pem',
-)
-
+  client = Mongo::Client.new(
+    ['localmongo1:27017'],
+    auth_mech. mongodb_x509,
+    ssl: true,
+    ssl_cert: '/etc/certs/mongodb/client.pem',
+    ssl_key: '/etc/certs/mongodb/client.pem',
+    ssl_ca_cert: '/etc/certs/mongodb/ca.pem'
+  )
 end
 # end x509 connection
