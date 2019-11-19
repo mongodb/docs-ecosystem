0894c83274a3d766c83f1a0e2fb7ee1758df7fac
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2019-11-19 13:33:26 -0500

    updated java connection snippet per spec

--- source/includes/connection-snippets/x509-snippets/java-connection.java
+++ source/includes/connection-snippets/x509-snippets/java-connection.java
@@ -1,34 +1,18 @@
 // begin x509 connection
-import java.util.Arrays;
-import org.bson.Document;
-import com.mongodb.MongoClientSettings;
-import com.mongodb.MongoCredential;
-import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoIterable;
 
 public class X509Connection {
 
-    public static void main(String args[]) {
-
+public static void main(String args[]) {
+        // Set system properties via commandline or programmatically
         System.setProperty("javax.net.ssl.keyStore", "/etc/certs/mongodb/v3/client.keystore");
         System.setProperty("javax.net.ssl.keyStorePassword", "<your_password>");
-
-        try {
-            String subject = "<your_subject>"; // x509 client certificate subject
-            MongoCredential credential = MongoCredential.createMongoX509Credential(subject);
-
-            MongoClientSettings settings = MongoClientSettings.builder().credential(credential)
-                    .applyToClusterSettings(builder -> builder.hosts(Arrays.asList(new ServerAddress("<cluster-url>"))))
-                    .applyToSslSettings(builder -> builder.enabled(true)).build();
-
-            MongoClient client = MongoClients.create(settings);
-            client.close();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-
+        
+        String uri = "mongodb+srv://<your-x509-client-subject>@<cluster-url>/?authMechanism=MONGODB-X509";
+        MongoClient client = MongoClients.create(uri);
+        // ...
+        client.close();
     }
 }
 // end x509 connection
