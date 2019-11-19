2aaecf73a2c6c32d748e9b47ec8b574dd0d5bab3
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2019-11-18 19:43:50 -0500

    added async syntax to node example

--- source/includes/connection-snippets/x509-snippets/nodejs-connection.js
+++ source/includes/connection-snippets/x509-snippets/nodejs-connection.js
@@ -1,20 +1,22 @@
 // begin x509 connection
-const MongoClient = require('mongodb').MongoClient;
+const { MongoClient } = require('mongodb');
 const fs = require('fs');
 
-const cert = fs.readFileSync("/etc/certs/mongodb/client.pem");
-const key = fs.readFileSync("/etc/certs/mongodb/client.pem");
-const ca = fs.readFileSync("/etc/certs/mongodb/ca.pem");
-
-const client = new MongoClient(`mongodb+srv://<cluster-url>?authMechanism=MONGODB-X509&ssl=true`, {
-    useNewUrlParser: true,
-    sslCA: ca,
-    sslKey: key,
-    sslCert: cert
+const client = new MongoClient('mongodb+srv://<cluster-url>?authMechanism=MONGODB-X509&ssl=true', {
+  sslCA: fs.readFileSync('/etc/certs/mongodb/ca.pem'),
+  sslKey: fs.readFileSync('/etc/certs/mongodb/client.pem'),
+  sslCert: fs.readFileSync('/etc/certs/mongodb/client.pem')
 });
 
-client.connect(function(err, db) {
+async function run() {
+  try {
+    await client.connect();
     // perform actions on the collection object
-    client.close();
-});
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
 // end x509 connection
