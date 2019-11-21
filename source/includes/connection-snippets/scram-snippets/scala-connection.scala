2045f86ce0c7dc1361e34415e18855dce39d4a25
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2019-11-20 19:32:55 -0500

    updated uri for scala to include streamType netty

--- source/includes/connection-snippets/scram-snippets/scala-connection.scala
+++ source/includes/connection-snippets/scram-snippets/scala-connection.scala
@@ -1,8 +1,7 @@
 // begin scram connection
 import org.mongodb.scala._
 
-val uri: String = "mongodb+srv://<username>:<password>@<cluster-url>/test?retryWrites=true&w=majority"
-System.setProperty("org.mongodb.async.type", "netty")
+val uri: String = "mongodb+srv://<username>:<password>@<cluster-url>/test?retryWrites=true&w=majority&streamType=netty"
 val client: MongoClient = MongoClient(uri)
 val db: MongoDatabase = client.getDatabase("test")
 // end scram connection
