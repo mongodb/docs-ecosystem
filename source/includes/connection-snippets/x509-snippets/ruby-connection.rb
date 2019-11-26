c1831e2c383a626957d9046e10ae318cdbc07609
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2019-11-26 18:54:01 -0500

    (DOCSP-7894): added ruby x509 snippet

---
+++ source/includes/connection-snippets/x509-snippets/ruby-connection.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# begin x509 connection
+require 'mongo'
+
+client = Mongo::Client.new(
+  ['<cluster-url>'],
+  auth_mech: :mongodb_x509,
+  ssl: true,
+  ssl_cert: '/etc/certs/mongodb/client.pem',
+  ssl_key: '/etc/certs/mongodb/client.pem'
+)
+db = client.database[:testDB]
+collection = client[:testCol]
+docCount = collection.count_documents({})
+print docCount
+# end x509 connection
