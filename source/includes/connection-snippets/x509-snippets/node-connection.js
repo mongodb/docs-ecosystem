e733cf3de075e502fe0134e2feb6a5eb837addc7
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2019-11-25 19:11:03 -0500

    (DOCSP-7890): added node x509 connection snippet

---
+++ source/includes/connection-snippets/x509-snippets/node-connection.js
@@ -0,0 +1,22 @@
+// begin x509 connection
+const { MongoClient } = require('mongodb');
+const fs = require('fs');
+
+const client = new MongoClient('mongodb+srv://<cluster-url>?authMechanism=MONGODB-X509&ssl=true', {
+  sslCA: fs.readFileSync('/etc/certs/mongodb/ca.pem'),
+  sslKey: fs.readFileSync('/etc/certs/mongodb/client.pem'),
+  sslCert: fs.readFileSync('/etc/certs/mongodb/client.pem')
+});
+
+async function run() {
+  try {
+    await client.connect();
+    // perform actions using client
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
+// end x509 connection
