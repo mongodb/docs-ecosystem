79a81428a14b1cd59080fc20f02412685945b772
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2019-12-03 10:10:36 -0500

    (DOCSP-7895): updated scala snip by cleaning imports and removing unneeded null

--- source/includes/connection-snippets/x509-snippets/scala-connection.scala
+++ source/includes/connection-snippets/x509-snippets/scala-connection.scala
@@ -1,35 +1,31 @@
 // begin x509 connection
-import org.mongodb.scala._
 import scala.collection.JavaConverters._
-import com.mongodb.connection.ClusterSettings
-import com.mongodb.connection.SslSettings
+import org.mongodb.scala._
 import org.mongodb.scala.bson.collection.mutable.Document
-import org.mongodb.scala.bson.BsonString
-import com.mongodb.client.model.FindOneAndUpdateOptions
+import org.mongodb.scala.connection.{ClusterSettings, SslSettings}
 
 object MongoDBx509 extends App {
 
-   System.setProperty("javax.net.ssl.keyStore", "client.keystore");
-   System.setProperty("javax.net.ssl.keyStorePassword", "<your_password>");
+  System.setProperty("javax.net.ssl.keyStore", "client.keystore");
+  System.setProperty("javax.net.ssl.keyStorePassword", "<your_password>");
 
-   // null value will force mongoDB server to use Subject from cert
-   val credential = MongoCredential.createMongoX509Credential(null);
+  val credential = MongoCredential.createMongoX509Credential()
 
-   val settings = MongoClientSettings.builder()
-      .credential(credential)
-      .applyToClusterSettings(
-         (builder: ClusterSettings.Builder)
-         => builder.hosts(List(new ServerAddress("<cluster-url>")).asJava))
-      .applyToSslSettings(
-         (builder : SslSettings.Builder)
-         => builder.enabled(true))
-      .build();
+  val settings = MongoClientSettings.builder()
+    .credential(credential)
+    .applyToClusterSettings(
+      (builder: ClusterSettings.Builder)
+      => builder.hosts(List(new ServerAddress("<cluster-url>")).asJava))
+    .applyToSslSettings(
+      (builder : SslSettings.Builder)
+      => builder.enabled(true))
+    .build()
 
-   val client : MongoClient = MongoClient(settings);
-   val database: MongoDatabase = client.getDatabase("testDB");
-   val collection: MongoCollection[Document] = database.getCollection("testCol");
+  val client : MongoClient = MongoClient(settings)
+  val database: MongoDatabase = client.getDatabase("testDB")
+  val collection: MongoCollection[Document] = database.getCollection("testCol")
 
-   val docCount = collection.countDocuments();
-   println(docCount);
+  val docCount = collection.countDocuments()
+  println(docCount)
 }
-// end x509 connection 
+// end x509 connection
