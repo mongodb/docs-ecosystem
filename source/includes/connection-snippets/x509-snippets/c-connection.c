b5fd04638424f4aaa16d1afd7e8c2dafbc8ca93f
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2019-12-16 17:03:47 -0500

    updated c code with proper formatting using clang-format -style=file -i

--- source/includes/connection-snippets/x509-snippets/c-connection.c
+++ source/includes/connection-snippets/x509-snippets/c-connection.c
@@ -2,31 +2,32 @@
 #include <mongoc/mongoc.h>
 #include <stdio.h>
 
-int
-main (int argc, char *argv[])
-{
-   mongoc_collection_t *collection;
-   mongoc_client_t *client;
-   int64_t count;
+int main(int argc, char *argv[]) {
+  mongoc_collection_t *collection;
+  mongoc_client_t *client;
+  int64_t count;
 
-   mongoc_init ();
+  mongoc_init();
 
-   client = mongoc_client_new ("mongodb+srv://<cluster-url>/test?authSource=$external&tlsCertificateKeyFile=/etc/certs/mongodb/client.pem&retryWrites=true&w=majority&authMechanism=MONGODB-X509");
+  client = mongoc_client_new(
+      "mongodb+srv://<cluster-url>/"
+      "test?authSource=$external&tlsCertificateKeyFile=/etc/certs/mongodb/"
+      "client.pem&retryWrites=true&w=majority&authMechanism=MONGODB-X509");
 
-   collection = mongoc_client_get_collection (client, "testDB", "testCol");
-   bson_t filter = BSON_INITIALIZER;
-   count = mongoc_collection_count_documents (collection, &filter, NULL, NULL, NULL, NULL);
-   if(count == -1){
-      printf("error occurred with count_documents");
-   }else{
-      printf ("%" PRId64 " documents counted.\n", count);
+  collection = mongoc_client_get_collection(client, "testDB", "testCol");
+  bson_t filter = BSON_INITIALIZER;
+  count = mongoc_collection_count_documents(collection, &filter, NULL, NULL,
+                                            NULL, NULL);
+  if (count == -1) {
+    printf("error occurred with count_documents");
+  } else {
+    printf("%" PRId64 " documents counted.\n", count);
+  }
+  bson_destroy(&filter);
+  mongoc_collection_destroy(collection);
+  mongoc_client_destroy(client);
+  mongoc_cleanup();
 
-   }
-   bson_destroy (&filter);
-   mongoc_collection_destroy (collection);
-   mongoc_client_destroy (client);
-   mongoc_cleanup ();
-
-   return 0;
+  return 0;
 }
 // end x509 connection
