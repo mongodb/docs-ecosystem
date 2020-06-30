43984b7c8b59d1384361cdda0e3b834ac84fdaa8
Chris Cho <chris.cho@10gen.com>
2020-06-30 14:40:29 -0400

    add print statement, imports

--- source/includes/connection-snippets/go-connection.go
+++ source/includes/connection-snippets/go-connection.go
@@ -1,12 +1,19 @@
 package main
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"log"
+	"time"
+)
 
 func main() {
 
 	// Replace the uri string with your MongoDB deployment's connection string.
 	uri := "mongodb+srv://<username>:<password>@<cluster-address>/test?w=majority"
-
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
@@ -18,6 +25,8 @@ func main() {
 	if err := client.Ping(ctx, readpref.Primary()); err != nil {
 		panic(err)
 	}
+
+	fmt.Println("Successfully connected and pinged.")
 
 	// Defer a function call to Disconnect and check for an error in case the
 	// application logs a warning instead of a panic.
