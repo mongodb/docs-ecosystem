3747a8fa60e76632f4c77cb3f6010858ab2519d9
Chris Cho <chris.cho@mongodb.com>
2023-03-02 16:58:49 -0500

    source

--- source/includes/connection-snippets/scram/c-connection-no-stable-api.c
+++ source/includes/connection-snippets/scram/c-connection-no-stable-api.c
@@ -1,23 +1,18 @@
 #include <mongoc/mongoc.h>
 
-int
-main (int argc, char *argv[])
+int main (int argc, char *argv[])
 {
    mongoc_database_t *database;
    mongoc_client_t *client;
-
+   bson_error_t error;
    mongoc_init ();
-
    // Replace the uri string with your MongoDB deployment's connection string.
    client = mongoc_client_new(
-      "mongodb+srv://<username>:<password>@<cluster-url>/test?retryWrites=true&w=majority"
+      "mongodb+srv://<connection string>"
    );
-   database = mongoc_client_get_database (client, "test");
-
+   database = mongoc_client_get_database (client, "admin");
    mongoc_database_destroy (database);
    mongoc_client_destroy (client);
-
    mongoc_cleanup ();
-
    return 0;
 }
