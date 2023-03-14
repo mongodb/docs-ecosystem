9dc2fce4ff97494163fb228fe43a4e53de813977
Chris Cho <chris.cho@mongodb.com>
2023-03-14 12:06:38 -0400

    update c and c++ stable api examples

--- source/includes/connection-snippets/scram/c-connection.c
+++ source/includes/connection-snippets/scram/c-connection.c
@@ -1,20 +1,35 @@
 #include <mongoc/mongoc.h>
 
-int main (int argc, char *argv[])
+int main (int argc, char **argv)
 {
-   mongoc_database_t *database;
-   mongoc_client_t *client;
-   bson_error_t error;
-   mongoc_init ();
-   // Replace the uri string with your MongoDB deployment's connection string.
-   client = mongoc_client_new(
-      "mongodb+srv://<connection string>"
-   );
-   api = mongoc_server_api_new (MONGOC_SERVER_API_V1);
-   mongoc_client_set_server_api (client, api, &error);
-   database = mongoc_client_get_database (client, "admin");
-   mongoc_database_destroy (database);
-   mongoc_client_destroy (client);
-   mongoc_cleanup ();
-   return 0;
+  mongoc_client_t *client = NULL;
+  bson_error_t error = {0};
+  mongoc_server_api_t *api = NULL;
+  mongoc_database_t *database = NULL;
+
+
+  // Initialize the MongoDB C Driver.
+  mongoc_init ();
+
+  // Replace the connection string with your MongoDB deployment's connection string.
+  client = mongoc_client_new("<connection string>");
+
+  // Set the version of the Stable API on the client.
+  api = mongoc_server_api_new (MONGOC_SERVER_API_V1);
+  if (!mongoc_client_set_server_api (client, api, &error))
+  {
+    // Error condition.
+    printf("%s", error.message);
+    return 0;
+  }
+
+  database = mongoc_client_get_database (client, "test");
+
+  // Perform Cleanup.
+  mongoc_database_destroy (database);
+  mongoc_client_destroy (client);
+  mongoc_cleanup ();
+
+  return 0;
 }
+
--- source/includes/connection-snippets/scram/cxx-connection.cpp
+++ source/includes/connection-snippets/scram/cxx-connection.cpp
@@ -5,27 +5,27 @@ int main()
 { 
   try
   {
-     // Create an instance.
-     mongocxx::instance inst{};
+    // Create an instance.
+    mongocxx::instance inst{};
   
-     // Replace the connection string with your MongoDB deployment's connection string.
-     const auto uri = mongocxx::uri{"<connection string>"};
+    // Replace the connection string with your MongoDB deployment's connection string.
+    const auto uri = mongocxx::uri{"<connection string>"};
   
-     // Set the version of the Stable API on the client.
-     mongocxx::options::client client_options;
-     auto api = mongocxx::options::server_api{ mongocxx::options::server_api::version::k_version_1 };
-     client_options.server_api_opts(api);
+    // Set the version of the Stable API on the client.
+    mongocxx::options::client client_options;
+    auto api = mongocxx::options::server_api{ mongocxx::options::server_api::version::k_version_1 };
+    client_options.server_api_opts(api);
   
-     // Setup the connection and get a handle on the database.
-     mongocxx::client conn{ uri, client_options};
-     mongocxx::database db = conn["test"];
-   }
+    // Setup the connection and get a handle on the database.
+    mongocxx::client conn{ uri, client_options};
+    mongocxx::database db = conn["test"];
+  }
 
   catch (const std::exception& e) 
   {
-     // Handle errors.
-     std::cout<< e.what() << std::endl;
-   }
+    // Handle errors.
+    std::cout<< e.what() << std::endl;
+  }
 
   return 0;
 }
