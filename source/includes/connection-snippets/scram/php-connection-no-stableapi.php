72096a40cced11cbb89b6c49373191d2c5a53264
Rea Radhika Rustagi <rea.rustagi@mongodb.com>
2023-03-14 10:19:52 -0400

    code updates

--- source/includes/connection-snippets/scram/php-connection-no-stableapi.php
+++ source/includes/connection-snippets/scram/php-connection-no-stableapi.php
@@ -1,11 +1,13 @@
 <?php
-
-require_once __DIR__ . '/vendor/autoload.php';
 use MongoDB\Client;
 
-$client = new Client('<connection string>');
+// Replace the placeholder with your Atlas connection string
+$uri = '<connection string>';
+// Create a new client and connect to the server
+$client = new Client($uri);
 
 try {
+    // Send a ping to confirm a successful connection
     $database = $client->admin;
     $cursor = $database->command(['ping' => 1]);
     echo "Pinged your deployment. You successfully connected to MongoDB!\n";
--- source/includes/connection-snippets/scram/php-connection.php
+++ source/includes/connection-snippets/scram/php-connection.php
@@ -1,13 +1,18 @@
 <?php
-
-require_once __DIR__ . '/vendor/autoload.php';
 use MongoDB\Client;
 use MongoDB\Driver\ServerApi;
 
+// Replace the placeholder with your Atlas connection string
+$uri = '<connection string>';
+
+// Specify Stable API version 1
 $serverApi = new ServerApi(ServerApi::V1);
-$client = new Client('<connection string>', [], ['serverApi' => $serverApi]);
+
+// Create a new client and connect to the server
+$client = new Client($uri, [], ['serverApi' => $serverApi]);
 
 try {
+    // Send a ping to confirm a successful connection
     $database = $client->admin;
     $cursor = $database->command(['ping' => 1]);
     echo "Pinged your deployment. You successfully connected to MongoDB!\n";
